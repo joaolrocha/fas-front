a297f05342c65f8f63807966b4e6e7fdce21765a
joaolrocha <joaolrocha07@gmail.com>
2024-07-22 12:10:06 -0300

    feat: add fields of form cliente

--- src/components/FormClient/FormClient.tsx
+++ src/components/FormClient/FormClient.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material'
 import './_formCliente.scss'
 
 const FormClient = () => {
@@ -14,8 +15,58 @@ const FormClient = () => {
             <h2>Dados de pessoa juridica</h2>
           </div>
 
-          <div className='form-client-container-body-PJ-body'>
-            
+          <div className='form-client-container-body-PJ-inputs'>
+            <TextField
+              id="outlined-basic"
+              label="CNPJ*"
+              variant="outlined" />
+            <TextField
+              id="outlined-basic"
+              label="Razão Social*"
+              variant="outlined" />
+            <TextField
+              id="outlined-basic"
+              label="Nome Fantasia*"
+              variant="outlined" />
+          </div>
+
+          <div className='form-client-container-body-address'>
+            <div className='form-client-container-body-address-title'>
+              <h2>Endereço</h2>
+            </div>
+
+            <div className='form-client-container-body-address-inputs'>
+              <TextField
+                id="outlined-basic"
+                label="Logradouro"
+                variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="Cidade"
+                variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="Estado"
+                variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="CEP"
+                variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="Bairro"
+                variant="outlined" />
+            </div>
+          </div>
+
+          <div className='form-client-container-body-contacts'>
+            <div className='form-client-container-body-contacts-title'>
+              <h2>Contatos</h2>
+            </div>
+
+            <div className='form-client-container-body-contacts-inputs'>
+              
+            </div>
           </div>
         </div>
 
