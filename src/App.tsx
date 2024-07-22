c5ae69e813d3d1adc41293e816e0bded113268b3
joaolrocha <joaolrocha07@gmail.com>
2024-07-22 09:46:50 -0300

    feat: config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './views/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Fas-front</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Login />}
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Login/Login.tsx
@@ -0,0 +1,7 @@
+import './_login.scss'
+
+export default function Login() {
+  return (
+    <h2>Login Page</h2>
+  )
+}
