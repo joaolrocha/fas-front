d13396b27ec1cc83c887112802acc1c66c67d607
joaolrocha <joaolrocha07@gmail.com>
2024-07-23 07:36:51 -0300

    feat: styling header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import logoFas from '../../assets/logo/logo.png'
 
+import './_header.scss'
+
 const Header = () => {
   return (
     <div className="header-component-container">
-        <div className='header-component-container-logo'>
-          <img src={logoFas} alt="logo fas" />
-        </div>
+      <div className='header-component-container-logo'>
+        <img src={logoFas} alt="logo fas" />
+      </div>
+
+      <div className='header-component-container-options'>
+        <p>Inicio</p>
+        <p>Cliente</p>
+        <p>Projeto</p>
+        <p>Recurso</p>
+        <p>Projeção</p>
+      </div>
     </div>
   )
 }
