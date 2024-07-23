1a6cedc0cd12c4cde90c83aeafcb61f4118278c9
joaolrocha <joaolrocha07@gmail.com>
2024-07-23 10:54:48 -0300

    feat: login screen form

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,7 +1,37 @@
+import { TextField } from '@mui/material'
 import './_login.scss'
 
 export default function Login() {
   return (
-    <h2>Login Page</h2>
+    <div className='login-view-container'>
+      <div className='login-view-content'>
+        <div className='login-view-content-title'>
+          <h2>Fas Solutions</h2>
+          <p>Portal de gestão de projetos</p>
+        </div>
+
+        <div className='login-view-content-form'>
+          <TextField
+            id="username"
+            label="Usuario"
+            variant="outlined"
+            sx={{ width: '100%' }}
+
+          />
+
+          <TextField
+            id="password"
+            label="Senha"
+            variant="outlined"
+            sx={{ width: '100%' }}
+
+          />
+        </div>
+
+        <div className='login-view-content-footer'>
+          <button className='button-system login'>Login</button>
+        </div>
+      </div>
+    </div>
   )
 }
